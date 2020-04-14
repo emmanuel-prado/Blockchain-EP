8472e31230d26454cf35bcbed7c80d3e06282446
Emmanium <pradoe.ep@gmail.com>
2020-04-14 16:46:31 -0600

    removed of work and valid proof from blockchain.py and added instead to miner.py

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -80,43 +80,6 @@ class Blockchain(object):
     def last_block(self):
         return self.chain[-1]
 
-    def proof_of_work(self, block):
-        """
-        Simple Proof of Work Algorithm
-        Stringify the block and look for a proof.
-        Loop through possibilities, checking each one against `valid_proof`
-        in an effort to find a number that is a valid proof
-        :return: A valid proof for the provided block
-        """
-        # TODO
-        block_string = json.dumps(block, sort_keys=True)
-        proof = 0
-        # return proof
-        while self.valid_proof(block_string, proof) is False:
-            proof += 1
-        return proof
-
-    @staticmethod
-    def valid_proof(block_string, proof):
-        """
-        Validates the Proof:  Does hash(block_string, proof) contain 3
-        leading zeroes?  Return true if the proof is valid
-        :param block_string: <string> The stringified block to use to
-        check in combination with `proof`
-        :param proof: <int?> The value that when combined with the
-        stringified previous block results in a hash that has the
-        correct number of leading zeroes.
-        :return: True if the resulting hash is a valid proof, False otherwise
-        """
-        # TODO
-        print(f"Checking if {proof} is valid")
-        guess = block_string + str(proof)
-        guess = guess.encode()
-        hash_value = hashlib.sha256(guess).hexdigest()
-        print(hash_value)
-        # return True or False
-        return hash_value[:3] == '000000'
-
 
 # Instantiate our Node
 app = Flask(__name__)
