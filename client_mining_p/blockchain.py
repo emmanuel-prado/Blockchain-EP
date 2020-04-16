91d1e9a92729072e0391e5888a98e02a023964b6
Emmanium <pradoe.ep@gmail.com>
2020-04-15 18:35:33 -0600

    added new_transactions to blockchain.py

--- client_mining_p/blockchain.py
+++ client_mining_p/blockchain.py
@@ -15,6 +15,24 @@ class Blockchain(object):
         # Create the genesis block
         self.new_block(previous_hash=1, proof=100)
 
+    def new_transactions(self, sender, recipient, amount):
+        """
+        Creates a new transaction to go into the next mined Block
+
+        :param sender: <str> Address of the Recipient
+        :param recipient: <str> Address of the Recipient
+        :param amount: <int> Amount
+        :return: <int> The index of the Block that will hold this transaction
+        """
+
+        self.current_transactions.append({
+            'sender': sender,
+            'recipient': recipient,
+            'amount': amount
+        })
+
+        return self.last_block['index'] + 1
+
     def new_block(self, proof, previous_hash=None):
         """
         Create a new Block in the Blockchain
