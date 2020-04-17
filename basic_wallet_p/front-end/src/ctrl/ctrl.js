b5b95f569d2b00b36248c8560e8a04159bab1935
Emmanium <pradoe.ep@gmail.com>
2020-04-17 17:06:21 -0600

    added more axios code

--- basic_wallet_p/front-end/src/ctrl/ctrl.js
+++ basic_wallet_p/front-end/src/ctrl/ctrl.js
@@ -1,15 +1,18 @@
 const axios = require('axios');
-
+//import axios from 'axios';
 // request blockchain data from python
-axios
-  .get('localhost:5000/chain')
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-  })
-  .then(function () {
-    // always executed
-  });
+const BASE_URL = 'localhost:5000';
+const getChain = async () => {
+  try {
+    response = axios.get(`${BASE_URL}/chain`);
+    chain = res.data;
+    console.log(`here's the chain: ${chain}`);
+    document.body.innerHTML = `<h2>${chain}</h2>`;
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+getChain();
+
+const applyData = async () => {};
